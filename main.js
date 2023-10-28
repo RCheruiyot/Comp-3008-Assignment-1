ee60865579dfc42780d0a4bec84c0051522f7f35
RCheruiyot <roycheruiyot.10@gmail.com>
2023-10-27 20:23:55 -0400

    navigating from page to page fix

--- main.js
+++ main.js
@@ -6,35 +6,18 @@ const proof = document.getElementById('fileInput')
 
 const displayPr = document.getElementById('Personal_Records')
 
-const profile_page_div = document.getElementById('profile_page_div')
-const groups_page_div = document.getElementById('profile_page_div')
-const leaderboard_div = document.getElementById('profile_page_div')
+// const profile_page_div = document.getElementById('profile_page_div')
+// const groups_page_div = document.getElementById('groups_page_div')
+// const leaderboard_div = document.getElementById('leaderboard_div')
 
 
-const profile_page_btn = document.getElementById('profile_page')
-const groups_page_btn = document.getElementById('groups_page')
-const leaderboard_btn = document.getElementById('leaderboard')
+// const profile_page_btn = document.getElementById('profile_page')
+// const groups_page_btn = document.getElementById('groups_page')
+// const leaderboard_btn = document.getElementById('leaderboard')
 
-profile_page_div.style.display = "block"; 
-groups_page_div.style.display = "block"; 
-leaderboard_div.style.display = "block"; 
 
-profile_page_btn.addEventListener("click", function() {
-  //profile_page_div.style.display = "block"; 
-  groups_page_div.style.display = "none"; 
-  leaderboard_div.style.display = "none"; 
-  
-});
-groups_page_btn.addEventListener("click", function() {
-  //groups_page_div.style.display = "block"; 
-  profile_page_div.style.display = "none"; 
-  //leaderboard_div.style.display = "none"; 
-});
-leaderboard_btn.addEventListener("click", function() {
-  //profile_page_div.style.display = "block"; 
-  leaderboard_div.style.display = "block"; 
-  //groups_page_div.style.display = "none"; 
-});
+
+
 
 pr.addEventListener('change', checkEnableButton)
 workout.addEventListener('change', checkEnableButton)
