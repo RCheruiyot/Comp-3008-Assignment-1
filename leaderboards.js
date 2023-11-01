ec97a79ff98dbfd96ddeece09ef27a50a346ca2a
Ahmed Abdou <ahmedfasla@gmail.com>
2023-11-01 16:25:27 -0400

    changed values for people using the app.

--- leaderboards.js
+++ leaderboards.js
@@ -1,9 +1,9 @@
 // Sample leaderboard data
 const people = [
-    { name: 'GuyL', benchpress: 150, squat: 200, deadlift: 250, group: 'school', image: 'assets/friend1.jpg' },
-    { name: 'GuyA', benchpress: 120, squat: 180, deadlift: 200, group: 'family', image: 'assets/friend2.jpg' },
-    { name: 'GuyB', benchpress: 160, squat: 210, deadlift: 260, group: 'work', image: 'assets/friend3.jpg' },
-    { name: 'GuyC', benchpress: 140, squat: 190, deadlift: 230, group: 'work', image: 'assets/friend4.jpg' },
+    { name: 'GuyL', benchpress: 225, squat: 190, deadlift: 200, group: 'school', image: 'assets/friend1.jpg' },
+    { name: 'GuyA', benchpress: 120, squat: 180, deadlift: 250, group: 'family', image: 'assets/friend2.jpg' },
+    { name: 'GuyB', benchpress: 160, squat: 210, deadlift: 180, group: 'work', image: 'assets/friend3.jpg' },
+    { name: 'GuyC', benchpress: 140, squat: 200, deadlift: 260, group: 'work', image: 'assets/friend4.jpg' },
     // Add more people here with their respective images
 ];
 
