3f079d1092d3a04bd65d1bf67110b9508941fae0
Ahmed Abdou <ahmedfasla@gmail.com>
2023-10-31 21:42:10 -0400

    Fixed personal records bug

--- leaderboards.js
+++ leaderboards.js
@@ -1,9 +1,9 @@
 // Sample leaderboard data
 const people = [
-    { name: 'GuyL', deadlift: 300, pushups: 50, pullups: 20, group: 'school', image: 'assets/friend1.jpg' },
-    { name: 'GuyA', deadlift: 250, pushups: 60, pullups: 10, group: 'family', image: 'assets/friend2.jpg' },
-    { name: 'GuyB', deadlift: 350, pushups: 40, pullups: 15, group: 'work', image: 'assets/friend3.jpg' },
-    { name: 'GuyC', deadlift: 350, pushups: 40, pullups: 15, group: 'work', image: 'assets/friend4.jpg' },
+    { name: 'GuyL', benchpress: 150, squat: 200, deadlift: 250, group: 'school', image: 'assets/friend1.jpg' },
+    { name: 'GuyA', benchpress: 120, squat: 180, deadlift: 200, group: 'family', image: 'assets/friend2.jpg' },
+    { name: 'GuyB', benchpress: 160, squat: 210, deadlift: 260, group: 'work', image: 'assets/friend3.jpg' },
+    { name: 'GuyC', benchpress: 140, squat: 190, deadlift: 230, group: 'work', image: 'assets/friend4.jpg' },
     // Add more people here with their respective images
 ];
 
@@ -23,9 +23,9 @@ function createProfileElement(person) {
     profileInfo.classList.add('leaderboards-profile-info');
     profileInfo.innerHTML = `
         <p>${person.name}</p>
+        <p>Bench Press: ${person.benchpress}</p>
+        <p>Squats: ${person.squat}</p>
         <p>Deadlift: ${person.deadlift}</p>
-        <p>Push-ups: ${person.pushups}</p>
-        <p>Pull-ups: ${person.pullups}</p>
         <p>Group: ${person.group}</p>
     `;
     profile.appendChild(profileImage);
